a71d1cd92f209591bf99fa3da915655121f4f009
harsh <harsh.kikani@exhibytesolution.com>
2025-08-05 15:59:05 +0530

    added greet and print

--- index.py
+++ index.py
@@ -2,3 +2,5 @@ def hello():
     print("hhhheelllllooooo")
     
 hello()
+
+print("This is the end of the hello function.")
--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def hello():
     print("hello world")
     
 hello()
+
+def greet():
+    print("Greetings from the greet function!")
     
     
+greet()
+
